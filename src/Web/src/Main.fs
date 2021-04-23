c3b5ccbb7a7c619da27a6078c99d401e3d509472
Michal Padzik <padzikm@gmail.com>
2021-04-24 01:32:18 +0200

    setup elmish

--- src/Web/src/Main.fs
+++ src/Web/src/Main.fs
@@ -1,13 +1,25 @@
 module Main
 
-open Feliz
-open App
 open Browser.Dom
 open Fable.Core.JsInterop
 
 importSideEffects "./styles/global.scss"
 
-ReactDOM.render(
-    Components.Router(),
-    document.getElementById "app"
-)
+open Elmish
+open Elmish.Debug
+open Elmish.Navigation
+open Elmish.UrlParser
+open Elmish.HMR
+open CompanyName.MyMeetings.Web.App
+
+
+Program.mkProgram init update render
+//|> Program.toNavigable (parseHash Router.pageParser) urlUpdate
+#if DEBUG
+|> Program.withConsoleTrace
+#endif
+|> Program.withReactSynchronous "app"
+#if DEBUG
+|> Program.withDebugger
+#endif
+|> Program.run
