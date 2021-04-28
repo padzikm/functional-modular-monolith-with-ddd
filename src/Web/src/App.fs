eaf35aaf50d69fb24a6935572e454e0eed5fd6c2
Michal Padzik <padzikm@gmail.com>
2021-04-28 19:29:58 +0200

    fetch meeting details

--- src/Web/src/App.fs
+++ src/Web/src/App.fs
@@ -1,37 +1,70 @@
 module CompanyName.MyMeetings.Web.App
 
+open System
 open Elmish
 open Feliz
+open Thoth.Fetch
+open Thoth.Json
+open Fable.Core
+
+type MeetingDetails = { Id: string; Title: string }
 
 type State =
-    { Count: int }
+    | Init
+    | Loading
+    | Loaded of MeetingDetails
+    | LoadedProblem of string
 
 type Msg =
-    | Increment
-    | Decrement
+    | Load of Guid
+    | Success of MeetingDetails
+    | Failure of string
 
-let init() =
-    { Count = 0 }, Cmd.none
+let init () = Init, Cmd.none
 
 let update (msg: Msg) (state: State) =
     match msg with
-    | Increment ->
-        { state with Count = state.Count + 1 }, Cmd.none
+    | Load id ->
+        let load =
+            async {
+                printfn "zaczynam pobieranie"
 
-    | Decrement ->
-        { state with Count = state.Count - 1 }, Cmd.none
+                let! data =
+                    Fetch.tryGet<_, MeetingDetails>($"/api/meetings/{id}", caseStrategy = CaseStrategy.CamelCase)
+                    |> Async.AwaitPromise
+
+                printfn "koniec pobierania"
+                printfn "%O" data
+
+                return
+                    match data with
+                    | Ok m -> Success m
+                    | Error ex ->
+                        match ex with
+                        | PreparingRequestFailed e -> Failure e.Message
+                        | NetworkError e -> Failure e.Message
+                        | FetchFailed e -> Failure e.StatusText
+                        | DecodingFailed e -> Failure e
+            }
+
+        Loading, Cmd.OfAsync.result load
+    | Success m -> Loaded m, Cmd.none
+    | Failure e -> LoadedProblem e, Cmd.none
 
 let render (state: State) (dispatch: Msg -> unit) =
-  Html.div [
-    Html.button [
-      prop.onClick (fun _ -> dispatch Increment)
-      prop.text "Increment"
-    ]
-
-    Html.button [
-      prop.onClick (fun _ -> dispatch Decrement)
-      prop.text "Decrement"
-    ]
-
-    Html.h1 state.Count
-  ]
+    match state with
+    | Init -> Html.div [ Html.button [ prop.text "load"; prop.onClick(fun _ -> dispatch (Load (Guid.Parse("EA0231DF-12A7-475C-A1B1-53F9EF0297B7")))) ] ]
+    | Loading -> Html.div "loading"
+    | Loaded meetingDetails ->
+        React.fragment [ Html.h1 "meeting details"
+                         Html.div [ Html.label [ prop.htmlFor "id"; prop.text "id" ]
+                                    Html.input [ prop.id "id"
+                                                 prop.type'.text
+                                                 prop.readOnly true
+                                                 prop.value meetingDetails.Id ]
+                                    Html.label [ prop.htmlFor "title"; prop.text "title" ]
+                                    Html.input [ prop.id "title"
+                                                 prop.type'.text
+                                                 prop.readOnly true
+                                                 prop.value meetingDetails.Title ] ] ]
+    | LoadedProblem problem -> Html.div problem
